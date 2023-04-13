255db82ed5c2ce78ab24c8f14782b4aa4de08c65
Sameer Haider <43320143+sameer-haider@users.noreply.github.com>
2023-04-12 21:15:50 -0500

    Added cosine similiarty to match input to pattern. Also added code to read in xml file and extract patterns.

--- main.py
+++ main.py
@@ -1,20 +1,33 @@
 import aiml
+import xml.etree.ElementTree as ET
+from nlp import best_match
+
+# Load the XML file
+tree = ET.parse("brain.xml")
+
+# Find all patterns in the XML
+root = tree.getroot()
+patterns = []
+for category in root.findall("category"):
+    pattern = category.find("pattern").text
+    patterns.append(str(pattern))
 
 # Create kernel and load aiml
 kernel = aiml.Kernel()
 kernel.learn("brain.xml")
 
 # Set the bot's name
-kernel.setBotPredicate("name", "Interstellar_Bot")
+kernel.setBotPredicate("name", "TARS")
 
 # Enter the main interaction loop
 while True:
     # Get user input
     message = input("> ").upper()
-    print(message)
+    match = best_match(message, patterns)
+    print(match)
 
     # Get bot response
-    response = kernel.respond(message)
+    response = kernel.respond(match)
 
     # Print the bot's response
     print(response)
---
+++ nlp.py
@@ -0,0 +1,48 @@
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+
+
+def best_match(user_input, patterns) -> str:
+    # Tokenize, lemmatize and remove stopwords from the user input and patterns
+    stop_words = set(stopwords.words("english"))
+    wordnet_lemmatizer = WordNetLemmatizer()
+    user_input_tokens = [
+        wordnet_lemmatizer.lemmatize(word.lower())
+        for word in word_tokenize(user_input)
+        if word.isalnum() and word.lower() not in stop_words
+    ]
+    pattern_tokens = [
+        [
+            wordnet_lemmatizer.lemmatize(word.lower())
+            for word in word_tokenize(pattern)
+            if word.isalnum() and word.lower() not in stop_words
+        ]
+        for pattern in patterns
+    ]
+
+    # Calculate the TF-IDF vectors for the user input and patterns
+    all_tokens = user_input_tokens + [
+        token for pattern in pattern_tokens for token in pattern
+    ]
+    all_tokens = sorted(list(set(all_tokens)))
+    user_input_vector = np.zeros(len(all_tokens))
+    pattern_vectors = np.zeros((len(pattern_tokens), len(all_tokens)))
+    for i, token in enumerate(all_tokens):
+        if token in user_input_tokens:
+            user_input_vector[i] = 1.0 / len(user_input_tokens)
+        for j, pattern_token in enumerate(pattern_tokens):
+            if token in pattern_token:
+                pattern_vectors[j, i] = 1.0 / len(pattern_token)
+
+    # Calculate the cosine similarities between the user input and the patterns
+    similarity_scores = cosine_similarity([user_input_vector], pattern_vectors)[0]
+
+    # Find the index of the highest similarity score
+    closest_match_index = np.argmax(similarity_scores)
+
+    # Return the closest match as a string
+    closest_match = patterns[closest_match_index]
+    return closest_match
